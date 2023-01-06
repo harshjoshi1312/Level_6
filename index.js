d9228b1027e0568e88d7903d39bac7106c69ecac
harshjoshi1312 <115775581+harshjoshi1312@users.noreply.github.com>
2023-01-06 15:10:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 const app = require("./app");
-let port = 3000;
-if (typeof process.env.PORT !== "undefined") {
-  port = process.env.PORT;
-}
 
-app.listen(port, () => {
-  console.log("Started express server at port " + port);
+app.listen(3000, () => {
+  console.log("Starting express server at port 3000");
 });
